6d8d23d47d371a8935918ef507bbcf6b20b3ef24
auugustoliks <auugustoliks@localhost.localdomain>
2020-02-02 01:18:13 -0300

    formatacao concluida

---
+++ setup.py
@@ -0,0 +1,13 @@
+setup(
+  name='sample',
+  packages = ['mypackage'], # this must be the same as the name above
+  version='0.1.0',
+  description='CLI para facilitar e padronizar rotinas básicas de git.',
+  author='Carlos Neto <carlos.santos110@fatec.sp.gov.br>',
+  author_email='pypa-dev@googlegroups.com',
+  license='MIT',
+  url='https://github.com/augustoliks/cliks',   # The project's main homepage.
+  keywords=['cli', 'git', 'example'],         # arbitrary keywords
+  classifiers=[],
+)
+from distutils.core import setup
---
+++ src/app.py
@@ -0,0 +1,98 @@
+import PyInquirer
+from git import Repo
+
+
+repo = Repo('..')
+
+is_issue = str(repo.active_branch)[0].isdigit()
+
+if is_issue:
+    id_issue = str(repo.active_branch)[0]
+else:
+    id_issue = 'nt'
+
+# __ACTION(TOPIC): COMMIT_MSG #TICKET_NUM [!TAG ...]__
+
+TEMPLATE = '{action}({topic}): {message} #{issue} [!{tag}]'
+
+
+class Issue(PyInquirer.Validator):
+    def validate(self, document):
+        if not document.text == 'nt':
+            try:
+                int(document.text)
+            except ValueError:
+                raise PyInquirer.ValidationError(
+                    message='Please enter a number or #nt',
+                    cursor_position=len(document.text)
+                )
+
+
+class NoEmpty(PyInquirer.Validator):
+    def validate(self, document):
+        if len(document.text.split(' ')) < 2:
+            raise PyInquirer.ValidationError(
+                message='Commit message must have grant 2 words',
+                cursor_position=len(document.text)
+            )
+
+
+questions = [
+    {
+        'type': 'list',
+        'name': 'action',
+        'message': '>>> Select ACTION:    ',
+        'choices': [
+            {'name': 'feat:        new features, big improvement'},
+            {'name': 'fix:         bug fixes'},
+            {'name': 'refactor:    code refactoring, without any visual user change'},
+            {'name': 'chore:       any else but the app'},
+            {'name': 'test:        any kind of tests (functionnal, unitary...)'},
+            {'name': 'style:       cosmetic changes (spaces vs tab...)'},
+            {'name': 'docs:        documentation'}
+        ],
+        'filter': lambda v: v.split(':')[0]
+    },
+    {
+        'type': 'list',
+        'name': 'topic',
+        'message': '>>> Select TOPIC:     ',
+        'choices': [
+            {'name': 'dev:         developpers (API changes, refactors...)'},
+            {'name': 'usr:         final users (UI changes)'},
+            {'name': 'pkg:         packagers   (packaging changes)'},
+            {'name': 'test:        testers     (test only related changes)'}
+        ],
+        'filter': lambda v: v.split(':')[0]
+    },
+    {
+        'type': 'input',
+        'name': 'message',
+        'message': '>>> Type Commit message: ',
+        'validate': NoEmpty,
+    },
+    {
+        'type': 'input',
+        'name': 'issue',
+        'message': '>>> Select ID-ISSUE:',
+        'default': id_issue,
+        'validate': Issue
+    },
+    {
+        'type': 'list',
+        'name': 'tag',
+        'message': '>>> Select TOPIC:     ',
+        'choices': [
+            {'name': 'refactor:    obviously for refactoring code only'},
+            {'name': 'minor:       a very meaningless change (a typo, adding a comment)'},
+            {'name': 'cosmetic:    cosmetic driven change (re-indentation, 80-col...)'},
+            {'name': 'wip:         partial functionality but complete subfunctionality.'}
+        ],
+        'filter': lambda v: v.split(':')[0]
+    }
+]
+
+answers = PyInquirer.prompt(questions)
+
+print(answers)
+print(TEMPLATE.format_map(answers))
