d2ae6bbd3a9638b2446fe09f3577a92b264127fa
Carlos Neto <carlos.santos110@fatec.sp.gov.br>
2020-02-02 17:26:17 -0300

    chore(dev): teste do cli #nt [!wip]

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import PyInquirer
+import argparse
 from git import Repo
 
 
@@ -10,7 +11,6 @@ if is_issue:
 else:
     id_issue = 'nt'
 
-# __ACTION(TOPIC): COMMIT_MSG #TICKET_NUM [!TAG ...]__
 
 TEMPLATE = '{action}({topic}): {message} #{issue} [!{tag}]'
 
@@ -36,62 +36,80 @@ class NoEmpty(PyInquirer.Validator):
             )
 
 
-questions = [
-    {
-        'type': 'list',
-        'name': 'action',
-        'message': '>>> Select ACTION:    ',
-        'choices': [
-            {'name': 'feat:        new features, big improvement'},
-            {'name': 'fix:         bug fixes'},
-            {'name': 'refactor:    code refactoring, without any visual user change'},
-            {'name': 'chore:       any else but the app'},
-            {'name': 'test:        any kind of tests (functionnal, unitary...)'},
-            {'name': 'style:       cosmetic changes (spaces vs tab...)'},
-            {'name': 'docs:        documentation'}
-        ],
-        'filter': lambda v: v.split(':')[0]
-    },
-    {
-        'type': 'list',
-        'name': 'topic',
-        'message': '>>> Select TOPIC:     ',
-        'choices': [
-            {'name': 'dev:         developpers (API changes, refactors...)'},
-            {'name': 'usr:         final users (UI changes)'},
-            {'name': 'pkg:         packagers   (packaging changes)'},
-            {'name': 'test:        testers     (test only related changes)'}
-        ],
-        'filter': lambda v: v.split(':')[0]
-    },
-    {
-        'type': 'input',
-        'name': 'message',
-        'message': '>>> Type Commit message: ',
-        'validate': NoEmpty,
-    },
-    {
-        'type': 'input',
-        'name': 'issue',
-        'message': '>>> Select ID-ISSUE:',
-        'default': id_issue,
-        'validate': Issue
-    },
-    {
-        'type': 'list',
-        'name': 'tag',
-        'message': '>>> Select TOPIC:     ',
-        'choices': [
-            {'name': 'refactor:    obviously for refactoring code only'},
-            {'name': 'minor:       a very meaningless change (a typo, adding a comment)'},
-            {'name': 'cosmetic:    cosmetic driven change (re-indentation, 80-col...)'},
-            {'name': 'wip:         partial functionality but complete subfunctionality.'}
-        ],
-        'filter': lambda v: v.split(':')[0]
-    }
-]
+def get_commit_message():
+    questions = [
+        {
+            'type': 'list',
+            'name': 'action',
+            'message': '>>> Select ACTION:    ',
+            'choices': [
+                {'name': 'feat:        new features, big improvement'},
+                {'name': 'fix:         bug fixes'},
+                {'name': 'refactor:    code refactoring, without any visual user change'},
+                {'name': 'chore:       any else but the app'},
+                {'name': 'test:        any kind of tests (functionnal, unitary...)'},
+                {'name': 'style:       cosmetic changes (spaces vs tab...)'},
+                {'name': 'docs:        documentation'}
+            ],
+            'filter': lambda v: v.split(':')[0]
+        },
+        {
+            'type': 'list',
+            'name': 'topic',
+            'message': '>>> Select TOPIC:     ',
+            'choices': [
+                {'name': 'dev:         developpers (API changes, refactors...)'},
+                {'name': 'usr:         final users (UI changes)'},
+                {'name': 'pkg:         packagers   (packaging changes)'},
+                {'name': 'test:        testers     (test only related changes)'}
+            ],
+            'filter': lambda v: v.split(':')[0]
+        },
+        {
+            'type': 'input',
+            'name': 'message',
+            'message': '>>> Type Commit message: ',
+            'validate': NoEmpty,
+        },
+        {
+            'type': 'input',
+            'name': 'issue',
+            'message': '>>> Select ID-ISSUE:',
+            'default': id_issue,
+            'validate': Issue
+        },
+        {
+            'type': 'list',
+            'name': 'tag',
+            'message': '>>> Select TOPIC:     ',
+            'choices': [
+                {'name': 'refactor:    obviously for refactoring code only'},
+                {'name': 'minor:       a very meaningless change (a typo, adding a comment)'},
+                {'name': 'cosmetic:    cosmetic driven change (re-indentation, 80-col...)'},
+                {'name': 'wip:         partial functionality but complete subfunctionality.'}
+            ],
+            'filter': lambda v: v.split(':')[0]
+        }
+    ]
 
-answers = PyInquirer.prompt(questions)
+    answers = PyInquirer.prompt(questions)
 
-print(answers)
-print(TEMPLATE.format_map(answers))
+    return TEMPLATE.format_map(answers)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='CLI for facilitate and standardize basically git routines')
+
+    parser.add_argument(
+        '-c', '--commit',
+        action='store_true',
+        help='Description: Insert commit message',
+        required=False
+    )
+
+    args = parser.parse_args()
+
+    if args.commit:
+        commit_message = get_commit_message()
+        repo.git.add('.')
+        repo.git.commit('.', '-m', commit_message)
--- test.py
+++ test.py
@@ -3,4 +3,3 @@ from git import Repo
 repo = Repo('.')
 repo.git.add('.')
 repo.git.commit('.', '-m', 'carlos')
-pass
